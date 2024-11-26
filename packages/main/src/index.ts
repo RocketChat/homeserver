870a7dc288d01aedb5208b94bbb6940e1a9711a4
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-11-26 18:12:01 -0300

    add logging

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,4 +1,5 @@
 import { Elysia, t } from "elysia";
+import { logger } from "@bogeychan/elysia-logger";
 
 import {
   EndpointsByMethod,
@@ -33,70 +34,78 @@ const app = new Elysia();
 
 console.log(config);
 app
+  .use(
+    logger({
+      level: "debug",
+    })
+  )
   .get("/", () => "")
-  .get(
-    "/.well-known/matrix/server",
-    cache(
-      async () =>
-        config.signingKey.reduce(
-          async (json, signingKey) =>
-            signJson(await json, signingKey, config.name),
-          Promise.resolve({
-            server_name: config.name,
-            // 1 day
-            valid_until_ts: new Date().getTime() + 60 * 60 * 24 * 1000,
-            old_verify_keys: {},
-            verify_keys: {
-              ...Object.fromEntries(
-                config.signingKey.map(({ algorithm, version }) => [
-                  `${algorithm}:${version}`,
-                  {
-                    key: algorithm,
-                  },
-                ])
-              ),
-            },
-            signatures: {},
-          })
-        ),
-      1000 * 60 * 60
-    ),
+  .group("/_matrix/key/v2", (matrixKeyV2) =>
+    matrixKeyV2.get(
+      "/server",
+      cache(
+        async () =>
+          config.signingKey.reduce(
+            async (json, signingKey) =>
+              signJson(await json, signingKey, config.name),
+            Promise.resolve({
+              server_name: config.name,
+              // 1 day
+              valid_until_ts: new Date().getTime() + 60 * 60 * 24 * 1000,
+              old_verify_keys: {},
+              verify_keys: {
+                ...Object.fromEntries(
+                  config.signingKey.map(({ algorithm, version }) => [
+                    `${algorithm}:${version}`,
+                    {
+                      key: algorithm,
+                    },
+                  ])
+                ),
+              },
+              signatures: {},
+            })
+          ),
+        1000 * 60 * 60
+      ),
 
-    {
-      response: {
-        200: t.Object({
-          server_name: t.String(),
-          valid_until_ts: t.Number(),
-          old_verify_keys: t.Record(
-            t.String(),
-            t.Object({
-              expired_ts: t.Number(),
-              key: t.String(),
-            })
-          ),
-          verify_keys: t.Record(
-            t.String(),
-            t.Object({
-              key: t.String(),
-            })
-          ),
-          signatures: t.Record(
-            t.String(),
-            t.Record(
+      {
+        response: {
+          200: t.Object({
+            server_name: t.String(),
+            valid_until_ts: t.Number(),
+            old_verify_keys: t.Record(
               t.String(),
-              t.String({
-                description:
-                  "A signature of the server's public key by the key id",
+              t.Object({
+                expired_ts: t.Number(),
+                key: t.String(),
               })
             ),
-            {
-              description: `Digital signatures for this object signed using the verify_keys. The signature is calculated using the process described at Signing JSON`,
-            }
-          ),
-        }),
-      },
-    }
+            verify_keys: t.Record(
+              t.String(),
+              t.Object({
+                key: t.String(),
+              })
+            ),
+            signatures: t.Record(
+              t.String(),
+              t.Record(
+                t.String(),
+                t.String({
+                  description:
+                    "A signature of the server's public key by the key id",
+                })
+              ),
+              {
+                description: `Digital signatures for this object signed using the verify_keys. The signature is calculated using the process described at Signing JSON`,
+              }
+            ),
+          }),
+        },
+      }
+    )
   )
+
   .listen(config.port);
 
 console.log(
