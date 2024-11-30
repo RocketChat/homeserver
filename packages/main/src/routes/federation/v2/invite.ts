c6253a3f3c4b7edf5810c7fcf92c0a024dddbc98
Diego Sampaio <chinello@gmail.com>
2024-11-30 12:19:08 -0300

    send_join

--- packages/main/src/routes/federation/v2/invite.ts
+++ packages/main/src/routes/federation/v2/invite.ts
@@ -6,34 +6,46 @@ import { config } from "../../../config";
 import { signJson } from "../../../signJson";
 import { authorizationHeaders } from "../../../authentication";
 
+const makeRequest = async ({ method, origin, uri, options = {} }: { method: string; origin: string; uri: string; options?: {}; }) => {
+  const auth = await authorizationHeaders(
+    config.name,
+    config.signingKey[0],
+    origin,
+    method,
+    uri
+  );
+
+  console.log("auth ->", auth);
+
+  return fetch(`https://${origin}${uri}`, {
+    ...options,
+    method,
+    headers: {
+      Authorization: auth,
+    },
+  });
+}
+
 export const inviteEndpoint = new Elysia().put(
   "/invite/:roomId/:eventId",
   ({ params, body }) => {
     setTimeout(async () => {
       const { event } = body as any;
 
-      const uri = `/_matrix/federation/v1/make_join/${params.roomId}/${event.state_key}?ver=10`;
+      const response = await makeRequest({ method: 'GET', origin: event.origin, uri: `/_matrix/federation/v1/make_join/${params.roomId}/${event.state_key}?ver=10` });
 
-      const auth = await authorizationHeaders(
-        config.name,
-        config.signingKey[0],
-        event.origin,
-        "GET",
-        uri
-      );
+      const responseMake = await response.json();
+      console.log("make_join ->", responseMake);
 
-      console.log("auth ->", auth);
+      const responseSend = await makeRequest({
+          method: 'PUT', origin: event.origin, uri: `/_matrix/federation/v1/send_join/${params.roomId}/${event.state_key}?ver=10`, options: {
+            body: JSON.stringify(responseMake.event),
+          }
+        });
 
-      const response = await fetch(`https://${event.origin}${uri}`, {
-        method: "GET",
-        headers: {
-          Authorization: auth,
-        },
-      });
+      const responseBody = await responseSend.json();
 
-      const responseBody = await response.json();
-
-      console.log("make_join ->", responseBody);
+      console.log("send_join ->", responseBody);
     }, 1000);
 
     return config.signingKey.reduce(
