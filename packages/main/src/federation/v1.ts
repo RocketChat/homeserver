2768ab0d771593c2765f15307ece405b2d458dc7
Tasso <tasso.evangelista@rocket.chat>
2024-11-30 22:30:39 -0300

    Use `prefix` option for endpoint groups

--- packages/main/src/federation/v1.ts
+++ packages/main/src/federation/v1.ts
@@ -1,22 +1,18 @@
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
 
-import "@hs/endpoints/src/query";
-import "@hs/endpoints/src/server";
 import { versionEndpoints } from "../routes/version/v1/version";
 import { usersEndpoints } from "../routes/users/v1/users";
 import { profileEndpoints } from "../routes/profile/v1/profile";
 
-export const v1Endpoints = new Elysia()
-    .group('_matrix/federation/v1', (matrixFederationV1) =>
-        matrixFederationV1.use(versionEndpoints)
-            .use(usersEndpoints)
-            .use(profileEndpoints)
-            .put('/send/:txnId', ({ params, body }) => {
-                console.log('receive send ->', params);
-                console.log('body ->', body);
+export const v1Endpoints = new Elysia({ prefix: "/_matrix/federation/v1" })
+  .use(versionEndpoints)
+  .use(usersEndpoints)
+  .use(profileEndpoints)
+  .put("/send/:txnId", ({ params, body }) => {
+    console.log("receive send ->", params);
+    console.log("body ->", body);
 
-                return {
-                    [params.txnId]: {},
-                };
-            })
-    )
+    return {
+      [params.txnId]: {},
+    };
+  });
--- packages/main/src/federation/v2.ts
+++ packages/main/src/federation/v2.ts
@@ -1,10 +1,7 @@
 import { Elysia } from "elysia";
 
-import "@hs/endpoints/src/query";
-import "@hs/endpoints/src/server";
 import { inviteEndpoint } from "../routes/invite/v2/invite";
 
-export const v2Endpoints = new Elysia()
-    .group('_matrix/federation/v2', (matrixFederationV2) =>
-        matrixFederationV2.use(inviteEndpoint)
-    )
+export const v2Endpoints = new Elysia({ prefix: "/_matrix/federation/v2" }).use(
+  inviteEndpoint
+);
