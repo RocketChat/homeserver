76ed8bfe5f29d2b596482a9180a0aad5a3d21454
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2024-12-02 09:43:54 -0300

    Remove unnecessary code

--- packages/main/src/events/m.room.create.ts
+++ packages/main/src/events/m.room.create.ts
@@ -21,13 +21,6 @@ export const roomCreateEvent = ({
 		state_key: "",
 		origin: sender.split(":").pop(),
 		origin_server_ts: ts,
-		// hashes: { sha256: "XFkxvgXOT9pGz5Hbdo7tLlVN2SmWhQ9ifgsbLio/FEo" },
-		// signatures: {
-		// 	hs1: {
-		// 		"ed25519:a_HDhg":
-		// 			"rmnvsWlTL+JP8Sk9767UR0svF4IrzC9zhUPbT+y4u31r/qtIaF9OtT1FP8tD/yFGD92qoTcRb4Oo8DRbLRXcAg",
-		// 	},
-		// },
 		unsigned: { age_ts: ts },
 	};
 };
