cd56c8af81845aea25bffc26fdbd70da216a1f3c
Tasso <tasso.evangelista@rocket.chat>
2024-12-01 23:51:54 -0300

    Get rid of `user_id` format

--- packages/main/src/dto.ts
+++ packages/main/src/dto.ts
@@ -1,30 +1,26 @@
-import { FormatRegistry } from "@sinclair/typebox";
 import { t } from "elysia";
 
 const MAX_USER_ID_LENGTH = 255;
 
-FormatRegistry.Set("user_id", isUserID);
+// function isUserID(userID: string) {
+// 	const match = userID.match(/^@(?<localpart>[^:]+):(?<domain>.+)$/);
 
-function isUserID(userID: string) {
-	const match = userID.match(/^@(?<localpart>[^:]+):(?<domain>.+)$/);
+// 	if (!match || !match.groups) {
+// 		return false;
+// 	}
 
-	if (!match || !match.groups) {
-		return false;
-	}
+// 	const { domain } = match.groups;
 
-	const { domain } = match.groups;
+// 	const matchDomain = domain.match(/^(?<hostname>[^:]+)(:(?<port>\d+))?$/);
 
-	const matchDomain = domain.match(/^(?<hostname>[^:]+)(:(?<port>\d+))?$/);
+// 	if (!matchDomain || !matchDomain.groups) {
+// 		return false;
+// 	}
 
-	if (!matchDomain || !matchDomain.groups) {
-		return false;
-	}
-
-	return true;
-}
+// 	return true;
+// }
 
 export const UserIDDTO = t.String({
-	format: "user_id",
 	maxLength: MAX_USER_ID_LENGTH,
 	error: "Invalid user ID format. Must be in the format '@localpart:domain'",
 });
