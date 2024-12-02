cb956d0ac4e533b95bcbc1e2150ac5024ec0e390
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2024-12-02 20:00:51 -0300

    Create initial events for room creation fake route

--- packages/main/src/routes/fake/room.ts
+++ packages/main/src/routes/fake/room.ts
@@ -7,6 +7,13 @@ import { signJson } from "../../signJson";
 import { computeHash, generateId } from "../../authentication";
 import { makeUnsignedRequest } from "../../makeRequest";
 import { pruneEventDict } from "../../pruneEventDict";
+import { roomCreateEvent } from "../../events/m.room.create";
+import { signEvent } from "../../signEvent";
+import { roomMemberEvent } from "../../events/m.room.member";
+import { roomPowerLevelsEvent } from "../../events/m.room.power_levels";
+import { roomJoinRulesEvent } from "../../events/m.room.join_rules";
+import { roomHistoryVisibilityEvent } from "../../events/m.room.history_visibility";
+import { roomGuestAccessEvent } from "../../events/m.room.guest_access";
 
 export const fakeEndpoints = new Elysia({ prefix: "/fake" })
 	.post("/sendMessage", async ({ body, error }) => {
@@ -99,46 +106,151 @@ export const fakeEndpoints = new Elysia({ prefix: "/fake" })
 	.post("/createRoom", async ({ body, error }) => {
 		const { username, sender } = body as any;
 
-		const event = {
-			auth_events: [],
-			prev_events: [],
-			type: "m.room.create",
-			depth: 1,
-			content: {},
-			origin: config.name,
-			origin_server_ts: Date.now(),
-			// room_id: roomId,
-			sender,
-		};
+		if (sender.split(":").pop() !== config.name) {
+			return error(400, "Invalid sender");
+		}
 
-		const payload = {
-			origin: config.name,
-			origin_server_ts: Date.now(),
-			pdus: [
-				{
-					...(await signJson(
-						pruneEventDict(computeHash(event)),
-						config.signingKey[0],
-						config.name,
-					)),
-					...event,
-				},
-			],
-		};
-		console.log("payload ->", payload);
+		const roomId = `!${generateId({
+			ts: Date.now(), // TEMP
+		})}:${config.name}`;
 
-		const response = await makeUnsignedRequest({
-			method: "PUT",
-			domain: username.split(":").pop(),
-			uri: `/_matrix/federation/v1/send/${Date.now()}`,
-			options: {
-				body: payload,
-			},
-		});
+		// Create
+		const createEvent = await signEvent(
+			roomCreateEvent({
+				roomId,
+				sender,
+			}),
+			config.signingKey[0],
+		);
 
-		console.log(response.status);
-		const responseMake = await response.json();
-		console.log("response ->", responseMake);
+		const createEventId = generateId(createEvent);
 
-		return responseMake;
+		console.log({ roomId, createEventId, createEvent });
+
+		// Member
+		const memberEvent = await signEvent(
+			roomMemberEvent({
+				roomId,
+				sender,
+				depth: 2,
+				auth_events: [createEventId],
+				prev_events: [createEventId],
+			}),
+			config.signingKey[0],
+		);
+
+		const memberEventId = generateId(memberEvent);
+
+		console.log({ roomId, memberEventId, memberEvent });
+
+		// PowerLevels
+		const powerLevelsEvent = await signEvent(
+			roomPowerLevelsEvent({
+				roomId,
+				sender,
+				member: username,
+				auth_events: [createEventId, memberEventId],
+				prev_events: [memberEventId],
+				depth: 3,
+			}),
+			config.signingKey[0],
+		);
+
+		const powerLevelsEventId = generateId(powerLevelsEvent);
+
+		console.log({ roomId, powerLevelsEventId, powerLevelsEvent });
+
+		// Join Rules
+		const joinRulesEvent = await signEvent(
+			roomJoinRulesEvent({
+				roomId,
+				sender,
+				auth_events: [createEventId, memberEventId, powerLevelsEventId],
+				prev_events: [powerLevelsEventId],
+				depth: 4,
+			}),
+			config.signingKey[0],
+		);
+
+		const joinRulesEventId = generateId(joinRulesEvent);
+
+		console.log({ roomId, joinRulesEventId, joinRulesEvent });
+
+		// History Visibility
+		const historyVisibilityEvent = await signEvent(
+			roomHistoryVisibilityEvent({
+				roomId,
+				sender,
+				auth_events: [
+					createEventId,
+					memberEventId,
+					powerLevelsEventId,
+					joinRulesEventId,
+				],
+				prev_events: [joinRulesEventId],
+				depth: 5,
+			}),
+			config.signingKey[0],
+		);
+
+		const historyVisibilityEventId = generateId(historyVisibilityEvent);
+
+		console.log({ roomId, historyVisibilityEventId, historyVisibilityEvent });
+
+		// Guest Access
+		const guestAccessEvent = await signEvent(
+			roomGuestAccessEvent({
+				roomId,
+				sender,
+				auth_events: [
+					createEventId,
+					memberEventId,
+					powerLevelsEventId,
+					joinRulesEventId,
+					historyVisibilityEventId,
+				],
+				prev_events: [historyVisibilityEventId],
+				depth: 6,
+			}),
+			config.signingKey[0],
+		);
+
+		const guestAccessEventId = generateId(guestAccessEvent);
+
+		console.log({ roomId, guestAccessEventId, guestAccessEvent });
+
+		// Member
+		// Message
+		// Member
+
+		// const payload = {
+		// 	origin: config.name,
+		// 	origin_server_ts: Date.now(),
+		// 	pdus: [
+		// 		{
+		// 			...(await signJson(
+		// 				pruneEventDict(computeHash(event)),
+		// 				config.signingKey[0],
+		// 				config.name,
+		// 			)),
+		// 			...event,
+		// 		},
+		// 	],
+		// };
+		// console.log("payload ->", payload);
+
+		// const response = await makeUnsignedRequest({
+		// 	method: "PUT",
+		// 	domain: username.split(":").pop(),
+		// 	uri: `/_matrix/federation/v1/send/${Date.now()}`,
+		// 	options: {
+		// 		body: payload,
+		// 	},
+		// });
+
+		// console.log(response.status);
+		// const responseMake = await response.json();
+		// console.log("response ->", responseMake);
+
+		// return responseMake;
 	});
