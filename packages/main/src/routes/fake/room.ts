aa7d754866b7d84152828681ea80e759190ad15a
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2024-12-03 11:47:20 -0300

    Add examples to fake endpoints

--- packages/main/src/routes/fake/room.ts
+++ packages/main/src/routes/fake/room.ts
@@ -148,166 +148,216 @@ const createRoom = async (sender: string, username: string) => {
 };
 
 export const fakeEndpoints = new Elysia({ prefix: "/fake" })
-	.post("/sendMessage", async ({ body, error }) => {
-		const { depth = 13, sender, roomId, msg, target } = body as any;
+	.post(
+		"/sendMessage",
+		async ({ body, error }) => {
+			const { depth = 13, sender, roomId, msg, target } = body as any;
 
-		const { events } = await import("../../mongodb");
+			const { events } = await import("../../mongodb");
 
-		const create = await events.findOne({
-			room_id: roomId,
-			type: "m.room.create",
-		});
+			const create = await events.findOne({
+				room_id: roomId,
+				type: "m.room.create",
+			});
 
-		// const powerLevels = await events.findOne({
-		// 	room_id: roomId,
-		// 	type: "m.room.power_levels",
-		// });
+			// const powerLevels = await events.findOne({
+			// 	room_id: roomId,
+			// 	type: "m.room.power_levels",
+			// });
 
-		const member = await events.findOne({
-			room_id: roomId,
-			type: "m.room.member",
-			"content.membership": "join",
-		});
+			const member = await events.findOne({
+				room_id: roomId,
+				type: "m.room.member",
+				"content.membership": "join",
+			});
 
-		const [last] = await events
-			.find(
-				{
-					room_id: roomId,
-				},
-				{ sort: { origin_server_ts: -1 }, limit: 1 },
-			)
-			.toArray();
-
-		if (!create || !member || !last) {
-			return error(400, "Invalid room_id");
-		}
-
-		create.event_id = generateId(create);
-		member.event_id = generateId(member);
-		last.event_id = generateId(last);
-		// powerLevels.event_id = generateId(powerLevels);
-
-		const event = {
-			auth_events: [
-				create.event_id,
-				// powerLevels.event_id,
-				member.event_id,
-			],
-			prev_events: [last.event_id],
-			type: "m.room.message",
-			depth,
-			content: {
-				body: msg,
-			},
-			origin: config.name,
-			origin_server_ts: Date.now(),
-			room_id: roomId,
-			sender,
-		};
-
-		const payload = {
-			origin: config.name,
-			origin_server_ts: Date.now(),
-			pdus: [
-				{
-					...(await signJson(
-						pruneEventDict(computeHash(event)),
-						config.signingKey[0],
-						config.name,
-					)),
-					...event,
-				},
-			],
-		};
-		console.log("payload ->", payload);
-
-		const response = await makeUnsignedRequest({
-			method: "PUT",
-			domain: target,
-			uri: `/_matrix/federation/v1/send/${Date.now()}`,
-			options: {
-				body: payload,
-			},
-		});
-
-		const responseMake = await response.json();
-		console.log("response ->", responseMake);
-
-		return responseMake;
-	})
-	.post("/createRoom", async ({ body, error }) => {
-		const { username, sender } = body as any;
-
-		if (sender.split(":").pop() !== config.name) {
-			return error(400, "Invalid sender");
-		}
-
-		const room = await createRoom(sender, username);
-
-		const lastEvent = room.guestAccessEvent as any; // TODO: Improve typing here
-
-		const payload = {
-			event: await signEvent(
-				{
-					auth_events: lastEvent.auth_events,
-					type: "m.room.member",
-					content: {
-						membership: "invite",
+			const [last] = await events
+				.find(
+					{
+						room_id: roomId,
 					},
-					depth: lastEvent.depth + 1,
-					origin: lastEvent.origin,
-					origin_server_ts: Date.now(),
-					prev_events: [room.guestAccessEventId], // TODO: Improve the return of the events to contain the event_id
-					room_id: room.roomId,
-					sender,
-					state_key: username,
-					unsigned: {
-						age: 4, // TODO: Check what this is
-					},
+					{ sort: { origin_server_ts: -1 }, limit: 1 },
+				)
+				.toArray();
+
+			if (!create || !member || !last) {
+				return error(400, "Invalid room_id");
+			}
+
+			create.event_id = generateId(create);
+			member.event_id = generateId(member);
+			last.event_id = generateId(last);
+			// powerLevels.event_id = generateId(powerLevels);
+
+			const event = {
+				auth_events: [
+					create.event_id,
+					// powerLevels.event_id,
+					member.event_id,
+				],
+				prev_events: [last.event_id],
+				type: "m.room.message",
+				depth,
+				content: {
+					body: msg,
 				},
-				config.signingKey[0],
+				origin: config.name,
+				origin_server_ts: Date.now(),
+				room_id: roomId,
+				sender,
+			};
+
+			const payload = {
+				origin: config.name,
+				origin_server_ts: Date.now(),
+				pdus: [
+					{
+						...(await signJson(
+							pruneEventDict(computeHash(event)),
+							config.signingKey[0],
+							config.name,
+						)),
+						...event,
+					},
+				],
+			};
+			console.log("payload ->", payload);
+
+			const response = await makeUnsignedRequest({
+				method: "PUT",
+				domain: target,
+				uri: `/_matrix/federation/v1/send/${Date.now()}`,
+				options: {
+					body: payload,
+				},
+			});
+
+			const responseMake = await response.json();
+			console.log("response ->", responseMake);
+
+			return responseMake;
+		},
+		{
+			body: t.Object(
+				{
+					sender: t.String(),
+					roomId: t.String(),
+					msg: t.String(),
+					target: t.String(),
+				},
+				{
+					examples: [
+						{
+							sender: "@a1:rc1",
+							roomId: "!uTqsSSWabZzthsSCNf:hs1",
+							msg: "My awoesome message",
+							target: "hs1",
+						},
+					],
+				},
 			),
-			invite_room_state: [
-				{
-					content: {},
-					sender,
-					state_key: "",
-					type: "m.room.join_rules",
-				},
-				{
-					content: {},
-					sender,
-					state_key: "",
-					type: "m.room.create",
-				},
-				{
-					content: {},
-					sender,
-					state_key: sender,
-					type: "m.room.member",
-				},
-			],
-			room_version: "10",
-		};
-
-		const eventId = generateId(payload.event);
-
-		console.log("payload ->", payload);
-		console.log("roomId ->", room.roomId);
-		console.log("eventId ->", eventId);
-
-		const response = await makeUnsignedRequest({
-			method: "PUT",
-			domain: username.split(":").pop(),
-			uri: `/_matrix/federation/v2/invite/${room.roomId}/${eventId}`,
-			options: {
-				body: payload,
+			detail: {
+				description:
+					"Send a message to a room. The sender must be the user ID. The target must be the server name.",
 			},
-		});
+		},
+	)
+	.post(
+		"/createRoom",
+		async ({ body, error }) => {
+			const { username, sender } = body as any;
 
-		console.log(response.status);
-		const responseMake = await response.json();
-		console.log("response ->", responseMake);
+			if (sender.split(":").pop() !== config.name) {
+				return error(400, "Invalid sender");
+			}
 
-		return responseMake;
-	});
+			const room = await createRoom(sender, username);
+
+			const lastEvent = room.guestAccessEvent as any; // TODO: Improve typing here
+
+			const payload = {
+				event: await signEvent(
+					{
+						auth_events: lastEvent.auth_events,
+						type: "m.room.member",
+						content: {
+							membership: "invite",
+						},
+						depth: lastEvent.depth + 1,
+						origin: lastEvent.origin,
+						origin_server_ts: Date.now(),
+						prev_events: [room.guestAccessEventId], // TODO: Improve the return of the events to contain the event_id
+						room_id: room.roomId,
+						sender,
+						state_key: username,
+						unsigned: {
+							age: 4, // TODO: Check what this is
+						},
+					},
+					config.signingKey[0],
+				),
+				invite_room_state: [
+					{
+						content: {},
+						sender,
+						state_key: "",
+						type: "m.room.join_rules",
+					},
+					{
+						content: {},
+						sender,
+						state_key: "",
+						type: "m.room.create",
+					},
+					{
+						content: {},
+						sender,
+						state_key: sender,
+						type: "m.room.member",
+					},
+				],
+				room_version: "10",
+			};
+
+			const eventId = generateId(payload.event);
+
+			console.log("payload ->", payload);
+			console.log("roomId ->", room.roomId);
+			console.log("eventId ->", eventId);
+
+			const response = await makeUnsignedRequest({
+				method: "PUT",
+				domain: username.split(":").pop(),
+				uri: `/_matrix/federation/v2/invite/${room.roomId}/${eventId}`,
+				options: {
+					body: payload,
+				},
+			});
+
+			console.log(response.status);
+			const responseMake = await response.json();
+			console.log("response ->", responseMake);
+
+			return responseMake;
+		},
+		{
+			body: t.Object(
+				{
+					username: t.String(),
+					sender: t.String(),
+				},
+				{
+					examples: [
+						{
+							username: "@admin:hs1",
+							sender: `@a${Math.floor(Math.random() * 100) + 1}:rc1`,
+						},
+					],
+				},
+			),
+			detail: {
+				description:
+					"Create a room and invite a user. The sender must be the server name. The username must be the user ID.",
+			},
+		},
+	);
