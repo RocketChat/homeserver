2367a186ec4927f81e7f3b077c96d7443fb1dd53
Guilherme Gazzo <guilhermegazzo@gmail.com>
2024-12-12 17:58:10 -0300

    chore: added minimal pdu validation

--- packages/homeserver/src/routes/federation/sendTransaction.ts
+++ packages/homeserver/src/routes/federation/sendTransaction.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import { isMongodbContext } from "../../plugins/isMongodbContext";
 import { generateId } from "../../authentication";
+import type { EventBase } from "@hs/core/src/events/eventBase";
 
 export const sendTransactionRoute = new Elysia().put(
 	"/send/:txnId",
@@ -16,11 +17,47 @@ export const sendTransactionRoute = new Elysia().put(
 			mongo: { eventsCollection },
 		} = context;
 
-		const { pdus } = body as any;
+		const { pdus, edu } = body as any;
 
-		if (pdus) {
+		if (edu.length > 100) {
+			throw new Error("Too many edus");
+		}
+
+		const isValidPDU = (pdu: any): pdu is EventBase => {
+			if (typeof pdu !== "object") {
+				return false;
+			}
+			if (typeof pdu.type !== "string") {
+				return false;
+			}
+			if (typeof pdu.room_id !== "string") {
+				return false;
+			}
+			if ("auth_events" in pdu && !Array.isArray(pdu.auth_events)) {
+				return false;
+			}
+			if ("prev_events" in pdu && !Array.isArray(pdu.prev_events)) {
+				return false;
+			}
+			return true;
+		};
+
+		const isValidPDUs = (pdus: any): pdus is EventBase[] => {
+			if (!Array.isArray(pdus)) {
+				return false;
+			}
+
+			if (pdus.length > 50) {
+				throw new Error("Too many pdus");
+			}
+
+			return pdus.every(isValidPDU);
+		};
+
+		// TODO: validate PDUs
+		if (isValidPDUs(pdus)) {
 			await eventsCollection.insertMany(
-				pdus.map((event: any) => ({
+				pdus.map((event) => ({
 					_id: generateId(event),
 					event,
 				})),
