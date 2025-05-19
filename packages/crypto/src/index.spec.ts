231a8b5cc71a5385ebc4841cd85401383d266c37
Debdut Chakraborty <debdutdeb@outlook.com>
2025-05-19 20:26:17 +0530

    move crypto stuff to package

---
+++ packages/crypto/src/index.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "bun:test";
+import { signJson } from ".";
+
+describe("signJson", () => {
+	it("should sign a json object", async () => {
+		const json = {
+			method: 'PUT',
+			uri: '/_matrix/federation/v1/send/1743489715804',
+			origin: 'syn1.tunnel.dev.rocket.chat',
+			destination: 'syn2.tunnel.dev.rocket.chat',
+			content: {"edus":[{"content":{"push":[{"last_active_ago":45931,"presence":"offline","user_id":"@debdut:syn1.tunnel.dev.rocket.chat"}]},"edu_type":"m.presence"}],"origin":"syn1.tunnel.dev.rocket.chat","origin_server_ts":1743490730808,"pdus":[]},
+			// signatures: {
+			// 	["syn1.tunnel.dev.rocket.chat"]: {
+			// 		["ed25519:a_FAET"]: "ZDz7K7NRz0OwgR6n96YMIyt9h8KUCb7T9TklId7S1UDVOwc2y45+tC12/51kxRxpUkaOgr+iBtSBBh74BIrsBQ",
+			// 	}
+			// }
+		}
+		
+		const seed = 'FC6cwY3DNmHo3B7GRugaHNyXz+TkBRVx8RvQH0kSZ04';
+
+		const signature = await signJson(json, seed);
+
+		expect(signature).toBe("ZDz7K7NRz0OwgR6n96YMIyt9h8KUCb7T9TklId7S1UDVOwc2y45+tC12/51kxRxpUkaOgr+iBtSBBh74BIrsBQ");
+	});
+});
