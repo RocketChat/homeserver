f90b2a3b0142641d20cc9802a6741e5662a7c779
Debdut Chakraborty <debdutdeb@outlook.com>
2025-05-20 20:00:36 +0530

    Revert "some formatting"
    This reverts commit 4f243979fd9dec39ae289b2490c28977c646ae40.

--- packages/homeserver/src/repositories/key.repository.ts
+++ packages/homeserver/src/repositories/key.repository.ts
@@ -1,6 +1,6 @@
-import { Inject, Injectable } from "@nestjs/common";
-import type { Collection, FindCursor, WithId } from "mongodb";
-import { DatabaseConnection } from "../database/database.connection";
+import { Inject, Injectable } from '@nestjs/common';
+import { Collection, FindCursor, WithId } from 'mongodb';
+import { DatabaseConnection } from '../database/database.connection';
 
 // a key document is unique by the server_name and verify_keys.[key_id]
 
@@ -24,63 +24,51 @@ export type ServerKey = {
 			key: string;
 		}
 	>;
-};
+}
 
 export type ServerKeyDocument = ServerKey & { _createdAt: Date };
 
 @Injectable()
 export class KeyRepository {
-	private collection: Collection<ServerKeyDocument> | null = null;
+    private collection: Collection<ServerKeyDocument> | null = null;
+    
+    constructor(
+        @Inject(DatabaseConnection) private readonly dbConnection: DatabaseConnection
+    ) {}
 
-	constructor(
-		@Inject(DatabaseConnection)
-		private readonly dbConnection: DatabaseConnection,
-	) {}
-
-	private async getCollection(): Promise<Collection<ServerKeyDocument>> {
-		if (!this.collection && !this.dbConnection) {
-			throw new Error("Database connection was not injected properly");
-		}
-
-		const db = await this.dbConnection.getDb();
-		this.collection = db.collection<ServerKeyDocument>("keys");
-		return this.collection;
-	}
-
+    private async getCollection(): Promise<Collection<ServerKeyDocument>> {
+        if (!this.collection && !this.dbConnection) {
+            throw new Error('Database connection was not injected properly');
+        }
+        
+        const db = await this.dbConnection.getDb();
+        this.collection = db.collection<ServerKeyDocument>('keys');
+        return this.collection;
+    }
+	
 	async storeKey(key: ServerKey): Promise<void> {
 		const collection = await this.getCollection();
 
 		await collection.insertOne({
-			...key,
-			_createdAt: new Date(),
+			...key, _createdAt: new Date(),
 		});
 	}
 
-	async findKey(
-		serverName: string,
-		keyId: string,
-		validUntil: number,
-	): Promise<WithId<ServerKeyDocument> | null> {
+	async findKey(serverName: string, keyId: string, validUntil: number): Promise<WithId<ServerKeyDocument> | null> {
 		const collection = await this.getCollection();
-		return collection.findOne(
-			{
-				server_name: serverName,
-				[`verify_keys.${keyId}`]: { $exists: true },
-				valid_until_ts: { $lte: validUntil },
-			},
-			{ sort: { valid_until_ts: -1 } },
-		);
+		return collection.findOne({
+			server_name: serverName,
+			[`verify_keys.${keyId}`]: { $exists: true },
+			valid_until_ts: { $lte: validUntil },
+		}, { sort: { valid_until_ts: -1 } });
 	}
-	async findKeys(
-		serverName: string,
-		keyId?: string,
-		validUntil?: number,
-	): Promise<FindCursor<WithId<ServerKeyDocument>>> {
+	async findKeys(serverName: string, keyId?: string, validUntil?: number): Promise<FindCursor<WithId<ServerKeyDocument>>> {
 		const collection = await this.getCollection();
 		return collection.find({
 			server_name: serverName,
 			...(keyId && { [`verify_keys.${keyId}`]: { $exists: true } }),
 			...(validUntil && { valid_until_ts: { $lte: validUntil } }),
-		});
+		})
 	}
-}
+
+} 
