7a98e2e0ebed6590d1a4a559c7a2dd6086c6d63d
Ricardo Garim <rswarovsky@gmail.com>
2025-05-26 10:46:02 -0300

    feat: send message reactions (#25)
    * feat: adds message reactions
    
    * feat: adds message reaction
    
    * adds commits suggestions

--- packages/homeserver/src/controllers/internal/message.controller.ts
+++ packages/homeserver/src/controllers/internal/message.controller.ts
@@ -1,12 +1,14 @@
+import type { ReactionEvent } from "@hs/core/src/events/m.reaction";
 import type { RoomMessageEvent } from "@hs/core/src/events/m.room.message";
 import {
   Body,
   Controller,
-  Post
+  Post,
 } from "@nestjs/common";
 import { z } from "zod";
 import { MessageService } from "../../services/message.service";
 import type { SignedEvent } from "../../signEvent";
+import { ZodValidationPipe } from '../../validation/pipes/zod-validation.pipe';
 
 const SendMessageSchema = z.object({
   roomId: z.string(),
@@ -17,12 +19,27 @@ const SendMessageSchema = z.object({
 
 type SendMessageResponseDto = SignedEvent<RoomMessageEvent>;
 
+const SendReactionSchema = z.object({
+  roomId: z.string(),
+  targetServer: z.string(),
+  eventId: z.string(),
+  emoji: z.string(),
+  senderUserId: z.string(),
+});
+
+type SendReactionResponseDto = SignedEvent<ReactionEvent>;
+
 @Controller("internal")
 export class InternalMessageController {
 	constructor(private readonly messageService: MessageService) {}
 
 	@Post("messages")
-  async sendMessage(@Body() body: z.infer<typeof SendMessageSchema>): Promise<SendMessageResponseDto> {
+  async sendMessage(@Body(new ZodValidationPipe(SendMessageSchema)) body: z.infer<typeof SendMessageSchema>): Promise<SendMessageResponseDto> {
     return this.messageService.sendMessage(body.roomId, body.message, body.senderUserId, body.targetServer);
+  }
+
+  @Post("reactions")
+  async sendReaction(@Body(new ZodValidationPipe(SendReactionSchema)) body: z.infer<typeof SendReactionSchema>): Promise<SendReactionResponseDto> {
+    return this.messageService.sendReaction(body.roomId, body.eventId, body.emoji, body.senderUserId, body.targetServer);
   }
 }
