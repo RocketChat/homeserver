31496a8b68f9a8651366bb2598c347b8044d7294
Debdut Chakraborty <debdutdeb@outlook.com>
2025-06-09 14:33:15 +0530

    revert event-state service change

--- packages/homeserver/src/services/event-state.service.ts
+++ packages/homeserver/src/services/event-state.service.ts
@@ -1,18 +1,9 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { StateEventRepository } from "../repositories/state-event.repository";
-import { resolveStateV2Plus } from "@hs/room/src/v2_resolution";
-import { V2Pdu } from "@hs/room/src/events";
-import { EventStore, EventStoreRemote } from "@hs/room/src/state_resolution/definitions/definitions";
-import { EventService } from "./event.service";
+import { createLogger } from '../utils/logger';
+import { injectable } from 'tsyringe';
 
-@Injectable()
+@injectable()
 export class EventStateService {
-	constructor(
-		private readonly stateEventRepository: StateEventRepository,
-		private readonly eventService: EventService,
-	) {}
-
-	private readonly logger = new Logger(EventStateService.name);
+	private readonly logger = createLogger('EventStateService');
 
 	async resolveState(roomId: string, eventId: string): Promise<void> {
 		this.logger.debug(
@@ -23,29 +14,6 @@ export class EventStateService {
 		// 1. Get the room state before the event
 		// 2. Apply state resolution algorithms if there are state conflicts
 		// 3. Update the room state in the database
-
-		const stateEvents = await (
-			await this.stateEventRepository.findByRoomId(roomId)
-		).toArray();
-		
-		const [{ event }] = await this.eventService.findEvents({ _id: eventId, roomId });
-
-		// FIXME: missing hashes
-		const state = await resolveStateV2Plus(stateEvents.concat(event) as unknown as V2Pdu[], {
-			store: {
-				getEvents: async (eventIds: string[]) => {
-					return (await this.eventService.getEventsByIds(eventIds)).map(
-						({ event }) => event,
-					) as unknown as V2Pdu[]; // hashes is missingh again
-				},
-			},
-			remote: {} as EventStoreRemote, // all evenrts should alreqdy be in store by this point1
-		});
-
-		await this.stateEventRepository.updateState(
-			roomId,
-			state.values().toArray(),
-		);
 
 		this.logger.debug(`State resolved for room ${roomId}`);
 	}
