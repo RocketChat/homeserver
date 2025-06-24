793925b77353b4b83c7c6a5cb2be8ea8be06ea39
Debdut Chakraborty <debdutdeb@outlook.com>
2025-06-24 16:48:27 +0530

    :)

--- packages/room/src/manager/factory.ts
+++ packages/room/src/manager/factory.ts
@@ -6,8 +6,8 @@ import {
 	PduTypeRoomPowerLevels,
 	type PduCreateEventContent,
 	type PduV1,
-	type PduTypeRoomName,
-	PduRoomNameEventContent,
+	PduTypeRoomName,
+	type PduRoomNameEventContent,
 } from '../types/v1';
 import type { PduV3 } from '../types/v3';
 import type { PduPowerLevelsEventV10Content, PduV10 } from '../types/v10';
