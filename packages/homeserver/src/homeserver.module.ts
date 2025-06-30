d34d0a0bf79ff12f54f3bf3a36fba6b6e086c7e9
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-06-30 12:56:00 -0300

    Move profiles.service.ts to federation-sdk and update import paths

--- packages/homeserver/src/homeserver.module.ts
+++ packages/homeserver/src/homeserver.module.ts
@@ -38,7 +38,7 @@ import { InviteService } from './services/invite.service';
 import { MessageService } from './services/message.service';
 import { MissingEventService } from '@hs/federation-sdk/src/services/missing-event.service';
 import { NotificationService } from '@hs/federation-sdk/src/services/notification.service';
-import { ProfilesService } from './services/profiles.service';
+import { ProfilesService } from '@hs/federation-sdk/src/services/profiles.service';
 import { RoomService } from './services/room.service';
 import { ServerService } from '@hs/federation-sdk/src/services/server.service';
 import { StateService } from './services/state.service';
---
+++ packages/homeserver/src/services.ts
@@ -0,0 +1,27 @@
+import { container } from 'tsyringe';
+import { RoomService } from './services/room.service';
+import { MessageService } from './services/message.service';
+import { EventService } from './services/event.service';
+import { InviteService } from './services/invite.service';
+import { WellKnownService } from '@hs/federation-sdk/src/services/well-known.service';
+import { ProfilesService } from '@hs/federation-sdk/src/services/profiles.service';
+
+export interface HomeserverServices {
+	room: RoomService;
+	message: MessageService;
+	event: EventService;
+	invite: InviteService;
+	wellKnown: WellKnownService;
+	profile: ProfilesService;
+}
+
+export function getAllServices(): HomeserverServices {
+	return {
+		room: container.resolve(RoomService),
+		message: container.resolve(MessageService),
+		event: container.resolve(EventService),
+		invite: container.resolve(InviteService),
+		wellKnown: container.resolve(WellKnownService),
+		profile: container.resolve(ProfilesService),
+	};
+}
