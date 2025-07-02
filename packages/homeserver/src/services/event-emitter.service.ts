f9f45f5f4d98b5eee711f835e2c8bb59e1f3b70b
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-07-02 11:11:12 -0300

    create EventEmitterService

---
+++ packages/homeserver/src/services/event-emitter.service.ts
@@ -0,0 +1,61 @@
+import {
+	Emitter,
+	type EventHandlerOf,
+	type EventOf,
+} from '@rocket.chat/emitter';
+import { injectable } from 'tsyringe';
+import type { HomeserverEventSignatures } from '../types/events';
+import logger from '../utils/logger';
+
+@injectable()
+export class EventEmitterService {
+	private emitter: Emitter<HomeserverEventSignatures> =
+		new Emitter<HomeserverEventSignatures>();
+
+	constructor() {}
+
+	public setEmitter(emitter: Emitter<HomeserverEventSignatures>): void {
+		this.emitter = emitter;
+		logger.info('EventEmitterService: External emitter injected');
+	}
+
+	public initializeStandalone(): void {
+		this.emitter = new Emitter<HomeserverEventSignatures>();
+		logger.info('EventEmitterService: Standalone emitter initialized');
+	}
+
+	public emit<K extends keyof HomeserverEventSignatures>(
+		event: K,
+		...[data]: EventOf<HomeserverEventSignatures, K> extends void
+			? [undefined?]
+			: [EventOf<HomeserverEventSignatures, K>]
+	): void {
+		this.emitter.emit(event, ...([data] as any));
+		logger.debug(`Event emitted: ${event}`, { event, data });
+	}
+
+	public on<K extends keyof HomeserverEventSignatures>(
+		event: K,
+		handler: EventHandlerOf<HomeserverEventSignatures, K>,
+	): (() => void) | undefined {
+		return this.emitter.on(event, handler);
+	}
+
+	public once<K extends keyof HomeserverEventSignatures>(
+		event: K,
+		handler: EventHandlerOf<HomeserverEventSignatures, K>,
+	): (() => void) | undefined {
+		return this.emitter.once(event, handler);
+	}
+
+	public off<K extends keyof HomeserverEventSignatures>(
+		event: K,
+		handler: EventHandlerOf<HomeserverEventSignatures, K>,
+	): void {
+		this.emitter.off(event, handler);
+	}
+
+	public getEmitter(): Emitter<HomeserverEventSignatures> {
+		return this.emitter;
+	}
+}
