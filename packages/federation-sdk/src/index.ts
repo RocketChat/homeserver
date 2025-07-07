f765d554801110ab2e1070520e836030edb96209
Debdut Chakraborty <debdutdeb@outlook.com>
2025-07-07 16:12:42 +0530

    this is why i don't like magic

--- packages/federation-sdk/src/index.ts
+++ packages/federation-sdk/src/index.ts
@@ -2,7 +2,6 @@ import type { Membership } from '@hs/core';
 import { container } from 'tsyringe';
 import { ConfigService } from './services/config.service';
 import { EventService } from './services/event.service';
-import { InviteService } from './services/invite.service';
 import { MessageService } from './services/message.service';
 import { ProfilesService } from './services/profiles.service';
 import { RoomService } from './services/room.service';
@@ -94,6 +93,8 @@ export { StateEventRepository } from './repositories/state-event.repository';
 export { ServerRepository } from './repositories/server.repository';
 export { KeyRepository } from './repositories/key.repository';
 export { StateRepository } from './repositories/state.repository';
+
+import { InviteService } from './services/invite.service';
 
 export interface HomeserverServices {
 	room: RoomService;
