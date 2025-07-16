c24ae5371ff6dca24e0097ad383fe76c4a13e261
Debdut Chakraborty <debdutdeb@outlook.com>
2025-07-16 14:50:05 +0530

    use new server-discovery everywhere

--- packages/federation-sdk/src/services/federation-request.service.spec.ts
+++ packages/federation-sdk/src/services/federation-request.service.spec.ts
@@ -33,9 +33,7 @@ describe('FederationRequestService', async () => {
 		},
 	];
 
-	const { getHomeserverFinalAddress } = await import(
-		'../server-discovery/discovery'
-	);
+	const { getHomeserverFinalAddress } = await import('@hs/core');
 
 	const { fetch: originalFetch } = await import('@hs/core');
 
@@ -56,7 +54,7 @@ describe('FederationRequestService', async () => {
 
 	afterAll(() => {
 		mock.restore();
-		mock.module('../server-discovery/discovery', () => ({
+		mock.module('@hs/core', () => ({
 			getHomeserverFinalAddress,
 		}));
 		mock.module('@hs/core', () => ({
