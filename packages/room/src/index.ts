1e3b1f3c0f9809fbbe6a187c2d20affa8c92b052
Debdut Chakraborty <debdutdeb@outlook.com>
2025-08-07 12:00:28 +0530

    export

--- packages/room/src/index.ts
+++ packages/room/src/index.ts
@@ -6,7 +6,9 @@ export { resolveStateV2Plus } from './state_resolution/definitions/algorithm/v2'
 export * from './manager/factory';
 export * from './manager/event-wrapper';
 export * from './manager/type';
+export * from './manager/room-state';
 export * from './state_resolution/definitions/definitions';
 
 export * from './types/v3-11';
 export * from './types/_common';
+
