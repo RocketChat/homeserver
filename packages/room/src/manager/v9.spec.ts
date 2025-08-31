e243dc8d52d49a44bf8317d216669e44179bbc80
Debdut Chakraborty <debdutdeb@outlook.com>
2025-08-31 15:30:16 +0530

    ..

--- packages/room/src/manager/v9.spec.ts
+++ packages/room/src/manager/v9.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'bun:test';
 
-import { PersistentEventV9 } from './v9';
 import { Pdu } from '../types/v3-11';
+import { PersistentEventV9 } from './v9';
 
 test('event without origin', async () => {
 	const event = new PersistentEventV9({
