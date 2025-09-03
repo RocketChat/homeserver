07f98b7263c08c54cf9a9a5845a0648d16ae8e23
Debdut Chakraborty <debdutdeb@outlook.com>
2025-09-03 15:02:27 +0530

    committing

--- packages/crypto/src/examples/signing-key.ssh-keygen.spec.ts
+++ packages/crypto/src/examples/signing-key.ssh-keygen.spec.ts
@@ -1,15 +1,42 @@
 import { describe, it } from 'node:test';
-import { getSshKeygenSigningKey } from './signing-key.ssh-keygen';
+// import { toUnpaddedBase64 } from '..';
+import { SshKeygenSigningKey } from './signing-key.ssh-keygen';
+import type { SigningKey } from '../signing-key';
+import { Ed25519SigningKeyImpl } from './ed25519';
+
+async function getsigningkey(): Promise<SigningKey> {
+	const signingKey = new SshKeygenSigningKey({
+		configDir: '/tmp',
+	});
+
+	await signingKey.load();
+
+	return signingKey;
+}
+
+async function getedsigningkey(): Promise<SigningKey> {
+	const signingkey = new Ed25519SigningKeyImpl('0');
+	await signingkey.load(new Uint8Array(32));
+	return signingkey;
+}
 
 describe('SshKeygenSigningKey', async () => {
-	const signingKey = await getSshKeygenSigningKey();
+	const signingKey = await getsigningkey();
 
 	it('should sign and verify data', async () => {
 		const data = 'Hello, World!';
 		const signature = await signingKey.sign(data);
-		const isValid = await signingKey.verify(data, signature);
-		if (!isValid) {
-			throw new Error('Signature verification failed');
-		}
+
+		// console.log(toUnpaddedBase64(signature));
+
+		await signingKey.verify(data, signature);
+	});
+
+	it('should work with crypto + ed keys', async () => {
+		const edsigningkey = await getedsigningkey();
+		const data = 'Hello, World!';
+		const signature = await edsigningkey.sign(data);
+
+		await edsigningkey.verify(data, signature);
 	});
 });
--- packages/crypto/src/signing-key.ts
+++ packages/crypto/src/signing-key.ts
@@ -1,24 +1,8 @@
-/*
- * From a seed to a SigningKey object.
- * The main thing to understand, are,
- * 1. the purpose is to get signature of a payload, generally a string or a Uint8Array - signing happens with the public key
- * 2. verify an existing signature - need the private key for this
- * ^ normal asynchronous cryptographic operations.
- * What changes are how the keys are stored and generated.
- * The easiest tool to use for that is ssh-keygen, for example `ssh-keygen -t ed25519 -C "your_email@example.com"`
- * This will generate a private and a public key pair.
- * An example of a SigningKey object that uses ssh-keygen is in
- */
-
-export enum EncryptionValidAlgorithm {
-	ed25519 = 'ed25519',
-
-	rsa = 'rsa', // NOT TO BE USED IN PRODUCTION
-}
-
-type Signature = Buffer;
-
-export type GenerateSigningKeyFunc = () => Promise<SigningKey>;
+import type {
+	DataType,
+	SignatureType,
+	EncryptionValidAlgorithm,
+} from './constants';
 
 export interface SigningKey {
 	// algorithm used, currently only ed25519 is supported
@@ -27,6 +11,6 @@ export interface SigningKey {
 	version: string;
 
 	// main implementors
-	sign(data: string | Buffer): Promise<Signature>;
-	verify(data: string | Buffer, signature: Signature): Promise<boolean>;
+	sign(data: DataType): Promise<SignatureType>;
+	verify(data: DataType, signature: SignatureType): Promise<void>; // throws if invalid
 }
