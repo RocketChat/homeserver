c5f0cac39cfbd660cbb9f9968861095704fceae5
Ricardo Garim <rswarovsky@gmail.com>
2025-09-03 11:56:16 -0300

    chore: removes todo from duplicated fields on event model (#140)

--- packages/core/src/models/event.model.ts
+++ packages/core/src/models/event.model.ts
@@ -11,7 +11,6 @@ export interface EventStore {
 	_id: string;
 	event: EventBaseWithOptionalId;
 
-	// TODO: remove the duplication of fields
 	staged?: boolean;
 
 	// TODO: check if this is needed or if we should create a new interface
