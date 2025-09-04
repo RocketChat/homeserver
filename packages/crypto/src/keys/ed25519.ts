9716d4041ef3d2c8577b6e1ff3468f38a52a3ef6
Debdut Chakraborty <debdutdeb@outlook.com>
2025-09-04 12:38:25 +0530

    ..

---
+++ packages/crypto/src/keys/ed25519.ts
@@ -0,0 +1,59 @@
+import crypto from 'node:crypto';
+
+import { EncryptionValidAlgorithm } from '../constants';
+import type { VerifierKey, Signer } from '../contracts/key';
+import {
+	ed25519PrivateKeyRawToPem,
+	ed25519PublicKeyRawToPem,
+} from '../rfc/8410/ed25519-pem';
+
+export class Ed25519VerifierKeyImpl implements VerifierKey {
+	algorithm = EncryptionValidAlgorithm.ed25519;
+
+	private _publicKeyPem: string;
+
+	constructor(
+		public version: string,
+		private readonly publicKey: Uint8Array,
+	) {
+		this._publicKeyPem = ed25519PublicKeyRawToPem(this.publicKey);
+	}
+
+	public async verify(data: Uint8Array, signature: Uint8Array): Promise<void> {
+		return new Promise((resolve, reject) => {
+			crypto.verify(null, data, this._publicKeyPem, signature, (err) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve();
+				}
+			});
+		});
+	}
+}
+
+export class Ed25519SigningKeyImpl
+	extends Ed25519VerifierKeyImpl
+	implements Signer
+{
+	public async sign(data: Uint8Array) {
+		return new Promise<Uint8Array>((resolve, reject) => {
+			crypto.sign(null, data, this._privateKeyPem, (err, signature) => {
+				if (err) {
+					return reject(err);
+				}
+				return resolve(signature);
+			});
+		});
+	}
+
+	private _privateKeyPem!: string;
+	constructor(
+		public version: string,
+		private readonly privateKey: Uint8Array,
+		publicKey: Uint8Array,
+	) {
+		super(version, publicKey);
+		this._privateKeyPem = ed25519PrivateKeyRawToPem(privateKey);
+	}
+}
