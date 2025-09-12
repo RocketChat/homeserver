4f9fe790f8d85bd510a2f77b8818d0d06bec7df3
Guilherme Gazzo <guilherme@gazzo.xyz>
2025-09-12 01:03:01 -0300

    lint

--- packages/federation-sdk/src/services/state.service.ts
+++ packages/federation-sdk/src/services/state.service.ts
@@ -840,7 +840,7 @@ export class StateService {
 			event.origin,
 			event.event,
 			event.eventId,
-			stateId
+			stateId,
 		);
 
 		// transactions not handled here, since we can use this method as part of a "transaction receive"
