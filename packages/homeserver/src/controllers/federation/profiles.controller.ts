4d647f35366c5469d6487c19d77268adc6a02cf7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-09-15 10:00:24 -0300

    feat: add state management DTOs and update profiles controller (#195)

--- packages/homeserver/src/controllers/federation/profiles.controller.ts
+++ packages/homeserver/src/controllers/federation/profiles.controller.ts
@@ -11,6 +11,12 @@ import {
 	GetMissingEventsBodyDto,
 	GetMissingEventsParamsDto,
 	GetMissingEventsResponseDto,
+	GetStateIdsParamsDto,
+	GetStateIdsQueryDto,
+	GetStateIdsResponseDto,
+	GetStateParamsDto,
+	GetStateQueryDto,
+	GetStateResponseDto,
 	MakeJoinParamsDto,
 	MakeJoinQueryDto,
 	MakeJoinResponseDto,
@@ -76,7 +82,7 @@ export const profilesPlugin = (app: Elysia) => {
 
 				const { ver } = query;
 
-				return profilesService.makeJoin(roomId, userId, ver ?? ['1']) as any;
+				return profilesService.makeJoin(roomId, userId, ver ?? ['1']);
 			},
 			{
 				params: MakeJoinParamsDto,
