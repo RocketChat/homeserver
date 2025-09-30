2b36eb049b505ef49e05119849f36b7f8d617b12
Debdut Chakraborty <debdutdeb@outlook.com>
2025-09-30 10:11:26 +0530

    chore: allow using v4 over v6

--- packages/federation-sdk/src/server-discovery/_resolver.ts
+++ packages/federation-sdk/src/server-discovery/_resolver.ts
@@ -1,7 +1,10 @@
+import type { LookupAllOptions } from 'node:dns';
 import { Resolver, lookup } from 'node:dns/promises';
 
 // no caching, depends on system
 class _Resolver extends Resolver {
+	private lookupOrder: LookupAllOptions['order'] = 'ipv6first';
+
 	constructor() {
 		super();
 
@@ -12,6 +15,19 @@ class _Resolver extends Resolver {
 
 			this.setServers(servers);
 		}
+
+		// spec says v6 first, but there shouldn't be a case where a and aaaa records point to different services. if does, not an application problem.
+		// for systems with no v6 support this still lets allow a fallback to v4 first. without needing to add system level dns filter.
+		// as long as the name has an a record, should be able to communicate.
+		const order = process.env
+			.HOMESERVER_CONFIG_DNS_LOOKUP_ORDER as typeof this.lookupOrder;
+		if (
+			order === 'ipv4first' ||
+			order === 'ipv6first' ||
+			order === 'verbatim'
+		) {
+			this.lookupOrder = order;
+		}
 	}
 
 	// The implementation uses an operating system facility that can associate names with addresses and vice versa
@@ -20,7 +36,7 @@ class _Resolver extends Resolver {
 		const result = await lookup(hostname, {
 			all: true,
 			family: 0,
-			order: 'ipv6first',
+			order: this.lookupOrder,
 		});
 
 		return result.map((r) => r.address);
