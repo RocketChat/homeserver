08911d3958345cb36c22068c52c4629277760c5b
Ricardo Garim <rswarovsky@gmail.com>
2025-11-16 22:57:14 -0300

    remove error from now found room name event

--- packages/room/src/manager/room-state.ts
+++ packages/room/src/manager/room-state.ts
@@ -66,8 +66,10 @@ export class RoomState {
 		const nameEvent = getStateByMapKey(this.stateMap, {
 			type: 'm.room.name',
 		});
+
+		// DMs do not have a name event, so we return undefined
 		if (!nameEvent || !nameEvent.isNameEvent()) {
-			throw new Error('Room name event not found');
+			return undefined;
 		}
 
 		return nameEvent.getContent().name;
