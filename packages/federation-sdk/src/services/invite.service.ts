e39d5b1b1cb3c6f40d04c8224a4ced1a99ef60ad
Guilherme Gazzo <guilhermegazzo@gmail.com>
2025-11-21 16:58:48 +0100

    fix: emit member events outside pdu process (#239)

--- packages/federation-sdk/src/services/invite.service.ts
+++ packages/federation-sdk/src/services/invite.service.ts
@@ -12,8 +12,14 @@ import {
 import { singleton } from 'tsyringe';
 import { ConfigService } from './config.service';
 import { EventAuthorizationService } from './event-authorization.service';
+import { EventEmitterService } from './event-emitter.service';
+import { EventService } from './event.service';
 import { FederationService } from './federation.service';
-import { StateService, UnknownRoomError } from './state.service';
+import {
+	RoomInfoNotReadyError,
+	StateService,
+	UnknownRoomError,
+} from './state.service';
 // TODO: Have better (detailed/specific) event input type
 export type ProcessInviteEvent = {
 	event: EventBase;
@@ -37,6 +43,7 @@ export class InviteService {
 		private readonly stateService: StateService,
 		private readonly configService: ConfigService,
 		private readonly eventAuthorizationService: EventAuthorizationService,
+		private readonly emitterService: EventEmitterService,
 	) {}
 
 	/**
@@ -103,6 +110,11 @@ export class InviteService {
 			// without it join events will not be processed if /event/{eventId} causes problems
 			void federationService.sendEventToAllServersInRoom(inviteEvent);
 
+			this.emitterService.emit('homeserver.matrix.membership', {
+				event_id: inviteEvent.eventId,
+				event: inviteEvent.event,
+			});
+
 			return {
 				event_id: inviteEvent.eventId,
 				event: PersistentEventFactory.createFromRawEvent(
@@ -132,6 +144,11 @@ export class InviteService {
 
 		// let everyone know
 		void federationService.sendEventToAllServersInRoom(inviteEvent);
+
+		this.emitterService.emit('homeserver.matrix.membership', {
+			event_id: inviteEvent.eventId,
+			event: inviteEvent.event,
+		});
 
 		return {
 			event_id: inviteEvent.eventId,
@@ -226,6 +243,11 @@ export class InviteService {
 
 			// we do not send transaction here
 			// the asking server will handle the transactions
+
+			this.emitterService.emit('homeserver.matrix.membership', {
+				event_id: inviteEvent.eventId,
+				event: inviteEvent.event,
+			});
 		}
 
 		// we are not the host of the server
