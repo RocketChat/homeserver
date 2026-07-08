2e8cb6a51b9d8788fc521aeff58d325ab1bdbf42
Diego Sampaio <chinello@gmail.com>
2026-07-08 16:14:13 -0300

    remove catch

--- packages/federation-sdk/src/services/event-sender.service.ts
+++ packages/federation-sdk/src/services/event-sender.service.ts
@@ -69,7 +69,7 @@ export class EventSenderService {
 		}
 
 		void this.federationService.sendEventToAllServersInRoom(event);
-		void this.eventRouterService.routePersistent(event).catch((e) => console.error(e));
+		void this.eventRouterService.routePersistent(event);
 
 		return event;
 	}
